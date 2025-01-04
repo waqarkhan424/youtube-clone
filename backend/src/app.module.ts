92a37953bbad74197f9a9e2802562b0846595ff8
Waqar Khan <waqar.mrwt424@gmail.com>
2025-01-04 19:53:39 +0500

    Your commit message

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { VideoModule } from './video/video.module';
 import { UserModule } from './user/user.module';
-import { ChannelModule } from './channel/channel.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.MONGODB_URI),
     VideoModule,
     UserModule,
-    ChannelModule,
   ],
   controllers: [],
   providers: [],
